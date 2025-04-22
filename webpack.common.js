384274ff5167c98c0e94c7fa5cbc24296048e1eb
meyieee <elizabetsiwy@gmail.com>
2025-04-22 10:52:53 +0800

    out file

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, "src/app.js"),
+  entry: path.join(__dirname, "notesappp/src/app.js"),
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
